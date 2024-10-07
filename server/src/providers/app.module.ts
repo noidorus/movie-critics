36197d9799325e6c0987376c5cb3ef975a1dfddb
Rodion <salevan99@yandex.ru>
2024-10-08 00:48:41 +0400

    DEV-S5: add cache to firebase service

--- server/src/providers/app.module.ts
+++ server/src/providers/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './firebase.module';
-import { HttpModule } from '@nestjs/axios';
 import { FilmsController } from 'src/controllers/films.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { SEC_60 } from 'src/constants/constants';
+
 @Module({
   imports: [
-    HttpModule,
     ConfigModule.forRoot({ cache: true, isGlobal: true }),
+    CacheModule.register({ isGlobal: true, ttl: SEC_60 }),
     FirebaseModule,
   ],
   controllers: [FilmsController],
--- server/src/services/firebase.service.ts
+++ server/src/services/firebase.service.ts
@@ -1,3 +1,5 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 import { Injectable, Inject } from '@nestjs/common';
 import { app, firestore } from 'firebase-admin';
 import { FilmsResponse, Film, Filters } from 'src/types/firebase.types';
@@ -11,6 +13,7 @@ export class FirebaseService {
   constructor(
     @Inject('FIREBASE_APP')
     private readonly firebaseApp: app.App,
+    @Inject(CACHE_MANAGER) private readonly cacheService: Cache,
   ) {
     this.#db = this.firebaseApp.firestore();
     this.#films = this.#db.collection('films');
@@ -24,15 +27,23 @@ export class FirebaseService {
     page?: number;
     limit?: number;
   }): Promise<FilmsResponse> {
-    console.log(page, limit);
     try {
+      const cachedResponse = await this.cacheService.get<FilmsResponse>(
+        `films:${page}:${limit}`,
+      );
+
+      if (cachedResponse) {
+        console.log('Films from cache');
+        return cachedResponse;
+      }
+
+      const count = await this.#films.count().get();
       const response = await this.#films
         .limit(limit)
         .offset((page - 1) * limit)
         .get();
-      const count = await this.#films.count().get();
 
-      return {
+      const data = {
         total: count.data().count,
         page,
         totalPages: Math.ceil(count.data().count / limit),
@@ -41,17 +52,29 @@ export class FirebaseService {
           return { ...data, id: doc.id };
         }),
       };
+
+      await this.cacheService.set(`films:${page}:${limit}`, data);
+      return data;
     } catch (err) {
       throw err;
     }
   }
 
-  async getFilters() {
-    const response = await this.#filters.get();
+  async getFilters(): Promise<Filters> {
+    const cachedResponse = await this.cacheService.get<Filters>('filters');
 
-    return response.docs.reduce<Filters>(
+    if (cachedResponse) {
+      console.log('Filters from cache');
+      return cachedResponse;
+    }
+
+    const response = await this.#filters.get();
+    const filters = response.docs.reduce<Filters>(
       (acc, val) => ({ ...acc, ...val.data() }),
       {},
     );
+
+    await this.cacheService.set('filters', filters);
+    return filters;
   }
 }
