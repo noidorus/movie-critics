b87c9c807366f23d9ca2f42e759e493a40fafc1a
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-10-29 20:38:14 +0700

    вынес логику из хука в стор

--- src/components/MoviesPageContent/hooks/useMoviesPage.ts
+++ src/components/MoviesPageContent/hooks/useMoviesPage.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '../../../store/hooks';
 import { fetchMovies } from '../../../store/Movies/moviesThunks';
 import {
@@ -7,7 +7,10 @@ import {
     selectCurrentPage,
     selectTotalPages,
     selectMoviesError,
+    selectIsFetching,
+    selectInitialLoad
 } from '../../../store/Movies/moviesSelector';
+import { setFetching } from '../../../store/Movies/moviesSlice';
 
 export const useMoviesPage = () => {
     const dispatch = useAppDispatch();
@@ -17,25 +20,24 @@ export const useMoviesPage = () => {
     const error = useAppSelector(selectMoviesError);
     const currentPage = useAppSelector(selectCurrentPage);
     const totalPages = useAppSelector(selectTotalPages);
-
-    const [isFetching, setIsFetching] = useState(false);
-    const isInitialLoad = useRef(true);
+    const isFetching = useAppSelector(selectIsFetching);
+    const initialLoad = useAppSelector(selectInitialLoad);
 
     useEffect(() => {
-        if (isInitialLoad.current) {
-            dispatch(fetchMovies(1));
-            isInitialLoad.current = false;
+        if (initialLoad) {
+            dispatch(fetchMovies(currentPage));
         }
-    }, [dispatch]);
+    }, [dispatch, currentPage, initialLoad]);
 
     const handleScroll = () => {
         const isBottomReached =
             window.innerHeight + document.documentElement.scrollTop >=
             document.documentElement.offsetHeight - 1;
-        if (!isBottomReached || isFetching || isLoading || currentPage >= totalPages) {
-            return;
-        }
-        setIsFetching(true);
+
+        if (!isBottomReached || isFetching || isLoading || currentPage >= totalPages) return;
+
+        dispatch(setFetching(true));
+        dispatch(fetchMovies(currentPage + 1)).then(() => dispatch(setFetching(false)));
     };
 
     useEffect(() => {
@@ -43,13 +45,5 @@ export const useMoviesPage = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, [isFetching, isLoading, currentPage, totalPages]);
 
-    useEffect(() => {
-        if (isFetching && currentPage < totalPages) {
-            dispatch(fetchMovies(currentPage + 1)).then(() => setIsFetching(false));
-        } else {
-            setIsFetching(false);
-        }
-    }, [isFetching, dispatch, currentPage, totalPages]);
-
-    return { movies, isLoading, error, currentPage, totalPages, isFetching };
+    return { movies, isLoading, error, isFetching };
 };
--- src/store/Movies/moviesSelector.ts
+++ src/store/Movies/moviesSelector.ts
@@ -9,3 +9,7 @@ export const selectMoviesError = (state: RootState) => state.movies.error;
 export const selectCurrentPage = (state: RootState) => state.movies.currentPage;
 
 export const selectTotalPages = (state: RootState) => state.movies.totalPages;
+
+export const selectIsFetching = (state: RootState) => state.movies.isFetching;
+
+export const selectInitialLoad = (state: RootState) => state.movies.initialLoad;
