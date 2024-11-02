6715907d909867e84784a6027ff2fefebba0c131
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-11-03 01:13:24 +0700

    stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+export default {
+    rules: {
+        'color-named': 'never',
+        'block-no-empty': true,
+        'no-duplicate-selectors': true,
+        'no-descending-specificity': true,
+        'rule-empty-line-before': 'always',
+        'declaration-empty-line-before': 'never',
+    },
+};
