364725ca3d02bf3825d2fa448d1481418bcdaf85
Rodion <salevan99@yandex.ru>
2024-11-06 17:52:11 +0400

    DEV-D1: fix env configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { FilmsModule } from './films/films.module';
-import configuration from './config/configuration';
+import config from './config/config';
 import { ListsModule } from './lists/lists.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ cache: true, isGlobal: true, load: [configuration] }),
+    ConfigModule.forRoot({ cache: true, isGlobal: true, load: [config] }),
     FilmsModule,
     AuthModule,
     ListsModule,
