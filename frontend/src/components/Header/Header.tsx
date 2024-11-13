fe5e559275576c0d32484e187939574611b7c03c
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-11-13 19:43:02 +0700

    собрал линки в хэдере через map

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,6 @@
-import { useCallback, useState } from 'react';
+import { useHeader } from './hooks/useHeader';
 import { Button } from 'primereact/button';
 import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { logoutUser } from '@/store/Auth/authThunks';
-import { selectUser } from '@/store/Auth/authSelectors';
 import classNames from 'classnames';
 import styles from './Header.module.css';
 
@@ -12,64 +9,40 @@ type HeaderProps = {
 };
 
 export default function Header({ activeTab }: HeaderProps) {
-    const dispatch = useAppDispatch();
-    const user = useAppSelector(selectUser);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
-    const handleLogout = useCallback(async () => {
-        await dispatch(logoutUser());
-    }, [dispatch]);
+    const {leftLinks, rightLinks, isMenuOpen, toggleMenu} = useHeader();
 
     return (
         <div className={styles.header}>
-                <p className={styles.logo}>MOVIE CRITICS</p>
-                <Button className={styles.menuButton} onClick={toggleMenu}>
-                    <span className={classNames('pi', 'pi-bars')}></span>
-                </Button>
-                <div className={classNames(styles.buttons, { [styles.open]: isMenuOpen })}>
-                    <div className={styles.leftButtons}>
+            <p className={styles.logo}>MOVIE CRITICS</p>
+            <Button className={styles.menuButton} onClick={toggleMenu}>
+                <span className={classNames('pi', 'pi-bars')}></span>
+            </Button>
+            <div className={classNames(styles.buttons, { [styles.open]: isMenuOpen })}>
+                <div className={styles.leftButtons}>
+                    {leftLinks.map(link => (
                         <Link
-                            to="/movies"
-                            className={classNames(styles.button, { [styles.buttonChecked]: activeTab === 'movies' })}
+                            key={link.key}
+                            to={link.to}
+                            className={classNames(styles.button, { [styles.buttonChecked]: activeTab === link.key })}
                         >
-                            <span>Фильмы</span>
+                            <span>{link.label}</span>
                         </Link>
-                        <Link
-                            to="/rating"
-                            className={classNames(styles.button, { [styles.buttonChecked]: activeTab === 'rating' })}
-                        >
-                            <span>Рейтинг</span>
-                        </Link>
-                        <Link
-                            to="/collections"
-                            className={classNames(styles.button, { [styles.buttonChecked]: activeTab === 'collections' })}
-                        >
-                            <span>Подборки</span>
-                        </Link>
-                    </div>
-                    <div className={styles.rightButtons}>
-                        {user ? (
-                            <>
-                                <Link
-                                    to="/collections/my"
-                                    className={classNames(styles.button, { [styles.buttonChecked]: activeTab === 'collections/my' })}
-                                >
-                                    <span>Мои подборки</span>
-                                </Link>
-                                <Link to="/login" className={styles.button} onClick={handleLogout}>
-                                    <span>Выйти</span>
-                                </Link>
-                            </>
-                        ) : (
-                            <Link to="/login" className={styles.button}>
-                                <span>Войти</span>
-                            </Link>
-                        )}
-                    </div>
+                    ))}
                 </div>
+                <div className={styles.rightButtons}>
+                    {rightLinks.map((link, index) => (
+                        <Link
+                            key={index}
+                            to={link.to}
+                            className={styles.button}
+                            onClick={link.onClick}
+                        >
+                            <span>{link.label}</span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
---
+++ frontend/src/types/LinkType.ts
@@ -0,0 +1,6 @@
+export type LinkType = {
+    to: string;
+    label: string;
+    key?: string;
+    onClick?: () => void;
+};
