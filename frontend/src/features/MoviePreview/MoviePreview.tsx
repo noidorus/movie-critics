acf5675adae30dba1f4ebd4a14e2678a74e34230
dima.khoroshev <horoshev.dimaa@gmail.com>
2024-11-18 19:21:02 +0700

    заменил a на Link

--- frontend/src/features/MoviePreview/MoviePreview.tsx
+++ frontend/src/features/MoviePreview/MoviePreview.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MoviePreviewData } from '@/app/types/MovieType';
 import styles from './MoviePreview.module.css';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 interface MoviePreviewProps {
     movie: MoviePreviewData;
@@ -19,9 +20,9 @@ const MoviePreview: React.FC<MoviePreviewProps> = ({ movie, deleteButton }) => {
                 <img src={movie.posterUrlPreview} alt={movie.nameRu} className={styles.poster} />
             </div>
             {deleteButton && <div className={styles.deleteButton}>{deleteButton}</div>}
-            <a className={styles.title} href={`/movies/${movie.id}`}>
+            <Link className={styles.title} to={`/movies/${movie.id}`}>
                 {movie.nameRu ? movie.nameRu : movie.nameOriginal}
-            </a>
+            </Link>
         </li>
     );
 };
