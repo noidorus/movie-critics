4f2625000bbadbef8c40ec91485d68374607c41a
Rodion <salevan99@yandex.ru>
2024-11-19 22:25:15 +0400

    Add swagger status codes to lists.controller, fix lists.service

--- backend/src/modules/lists/lists.controller.ts
+++ backend/src/modules/lists/lists.controller.ts
@@ -1,5 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ListsService } from './lists.service';
 import { RequestWithNullableUser, RequestWithUser } from '../auth/auth.intrfaces';
 import { JwtAuthGuard, NullableJwtAuthGuard } from '../auth/guards';
@@ -13,25 +25,30 @@ import { ShortInfoFilmEntity } from '../films/entities';
 export class ListsController {
   constructor(private readonly listsService: ListsService) {}
 
-  @Get()
-  @ApiOkResponse({ type: [ListWithAuthorAndShortFilms] })
   @ApiOperation({ summary: 'Get all public lists' })
+  @ApiResponse({ status: HttpStatus.OK, type: [ListWithAuthorAndShortFilms] })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
+  @Get()
   getLists(): Promise<ListWithAuthorAndShortFilms[]> {
     return this.listsService.getLists();
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get authenticated user lists' })
+  @ApiResponse({ status: HttpStatus.OK, type: [ListWithAuthorAndShortFilms] })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
   @Get('my')
-  @ApiOkResponse({ type: [ListWithAuthorAndShortFilms] })
-  @ApiOperation({ summary: 'Get my lists, available only for authorized users' })
   getMyLists(@Req() req: RequestWithUser): Promise<ListWithAuthorAndShortFilms[]> {
     return this.listsService.getMyLists(req.user.id);
   }
 
   @UseGuards(NullableJwtAuthGuard)
-  @Get(':id')
-  @ApiOkResponse({ type: ListWithAuthorAndFilms })
   @ApiOperation({ summary: 'Get list by id' })
+  @ApiResponse({ status: HttpStatus.OK, type: ListWithAuthorAndFilms })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'List not found' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
+  @Get(':id')
   getListById(
     @Req() req: RequestWithNullableUser,
     @Param('id', PositiveNumberValidationPipe) id: number,
@@ -40,17 +57,24 @@ export class ListsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Create list' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: ListEntity })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.CONFLICT, description: 'List with this name already exists' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
   @Post()
-  @ApiOkResponse({ type: ListEntity, description: 'List created' })
-  @ApiOperation({ summary: 'Create list, available only for authorized users' })
   createList(@Body() dto: CreateListDTO, @Req() req: RequestWithUser): Promise<ListEntity> {
     return this.listsService.createList(req.user.id, dto);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Post(':id/films/:filmId')
   @ApiOperation({ summary: 'Add film to list, available only for authorized users' })
-  @ApiOkResponse({ description: 'Film added to list', type: ShortInfoFilmEntity })
+  @ApiResponse({ status: HttpStatus.OK, type: ShortInfoFilmEntity })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'You have not access to this list' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Film not found' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
+  @Post(':id/films/:filmId')
   addFilmToList(
     @Req() req: RequestWithUser,
     @Param('id', PositiveNumberValidationPipe) id: number,
@@ -60,9 +84,12 @@ export class ListsController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Delete(':id')
   @ApiOperation({ summary: 'Delete list, available only for authorized users' })
-  @ApiOkResponse({ example: true, description: 'List deleted' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'List deleted' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
   removeList(
     @Param('id', PositiveNumberValidationPipe) id: number,
     @Req() req: RequestWithUser,
@@ -71,9 +98,13 @@ export class ListsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Remove film from list' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'Film removed from list' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'You have not access to this list' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id/films/:filmId')
-  @ApiOperation({ summary: 'Remove film from list, available only for authorized users' })
-  @ApiOkResponse({ description: 'Film removed from list' })
   removeFilmFromList(
     @Req() req: RequestWithUser,
     @Param('id', PositiveNumberValidationPipe) id: number,
@@ -83,13 +114,17 @@ export class ListsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Change list visibility' })
+  @ApiResponse({ status: HttpStatus.OK, type: ListEntity })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'You have not access to this list' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'List not found' })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: 'Something went wrong' })
   @Patch(':id/visibility')
-  @ApiOkResponse({ type: ListEntity })
-  @ApiOperation({ summary: 'Change list visibility, available only for authorized users' })
   changeListVisibility(
+    @Req() req: RequestWithUser,
     @Param('id', PositiveNumberValidationPipe) id: number,
     @Body() dto: ChangeListVisibilityDTO,
-    @Req() req: RequestWithUser,
   ): Promise<ListEntity> {
     return this.listsService.changeVisibility(req.user.id, id, dto.private);
   }
